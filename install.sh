f9b155aefd79c37ec3548cae620dd938a3768350
Erik Veld <eveld@xebia.com>
2015-11-02 20:37:53 +0100

    Altered some settings to work on linux

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 source run/.bash_profile
 
 # Set up symlinks.
-ln -sfv ".dotfiles/run/.bash_profile" ~
+if [ "$(uname)" == "Darwin" ]; then
+  ln -sfv ".dotfiles/run/.bash_profile" ~/.bash_profile
+else
+  ln -sfv ".dotfiles/run/.bash_profile" ~/.bashrc
+fi
+
 ln -sfv ".dotfiles/run/.inputrc" ~
 ln -sfv ".dotfiles/git/.gitconfig" ~
 ln -sfv ".dotfiles/git/.gitignore_global" ~
