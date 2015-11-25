0392165198d043b7ef94aee85b294eb0a9cf9486
Maarten van den Ende <maartenvdende@gmail.com>
2015-11-25 01:05:34 +0100

    Update dock.sh

--- osx/dock.sh
+++ osx/dock.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Mail.app"
+dockutil --no-restart --add "/Applications/Calendar.app"
 dockutil --no-restart --add "$HOME/Applications/Google Chrome.app"
 dockutil --no-restart --add "$HOME/Applications/iTerm.app"
 dockutil --no-restart --add "$HOME/Applications/Slack.app"
-dockutil --no-restart --add "$HOME/Applications/Atom.app"
-dockutil --no-restart --add "$HOME/Applications/Spotify.app"
 
 killall Dock
