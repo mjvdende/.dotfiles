2d18ea824ab5a505e2eda09a0583211ed2390718
Maarten <maartenvdende@gmail.com>
2022-09-06 08:34:36 +0200

    fix export nvm script

--- system/exports.zsh
+++ system/exports.zsh
@@ -15,8 +15,12 @@ export JAVA_HOME="$HOME/.sdkman/candidates/java/bin/current"
 # npm
 export PATH=~/.npm-global/bin:$PATH
 
-
-source $HOME/.rvm/scripts/rvm
+# Node
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
 
 # THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
